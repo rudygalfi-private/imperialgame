06fdc0c932aacfb9326a299cec43651eb88cdb4a
rudygalfi <rudygalfi@5acf17ae-e76b-11de-92aa-3ddf338522b3>
2009-12-13 02:24:37 +0000

    Updating all files based on StyleCop suggestions.

--- Bond.cs
+++ Bond.cs
@@ -1,8 +1,27 @@
-﻿namespace Imperial
+﻿// <copyright file="Bond.cs" company="Untitled">
+//     Copyright (c) Untitled. All rights reserved.
+// </copyright>
+
+namespace Imperial
 {
-    struct Bond
+    /// <summary>
+    /// Defines a bond.
+    /// </summary>
+    public struct Bond
     {
-        uint FaceValue;
-        uint InterestPayment;
+        /// <summary>
+        /// The name of the country that issued the bond.
+        /// </summary>
+        public CountryName IssuingCountry;
+
+        /// <summary>
+        /// The face value of the bond when bought.
+        /// </summary>
+        public uint FaceValue;
+
+        /// <summary>
+        /// The interest paid to the holder of the bond during the Investor phase.
+        /// </summary>
+        public uint InterestPayment;
     }
 }
--- Country.cs
+++ Country.cs
@@ -1,24 +1,83 @@
-﻿namespace Imperial
+﻿// <copyright file="Country.cs" company="Untitled">
+//     Copyright (c) Untitled. All rights reserved.
+// </copyright>
+
+namespace Imperial
 {
-    class Country
+    /// <summary>
+    /// Defines the country names.
+    /// </summary>
+    public enum CountryName : short
     {
-        private string name;
+        /// <summary>
+        /// Name of Austria-Hungary
+        /// </summary>
+        AustriaHungary,
+
+        /// <summary>
+        /// Name of Italy
+        /// </summary>
+        Italy,
+
+        /// <summary>
+        /// Name of France
+        /// </summary>
+        France,
+
+        /// <summary>
+        /// Name of United Kingdom
+        /// </summary>
+        UnitedKingdom,
+
+        /// <summary>
+        /// Name of Germany
+        /// </summary>
+        Germany,
+
+        /// <summary>
+        /// Name of Russia
+        /// </summary>
+        Russia
+    }
+
+    /// <summary>
+    /// Implements a country.
+    /// </summary>
+    public class Country
+    {
+        /// <summary>
+        /// The name of this country.
+        /// </summary>
+        private CountryName name;
+        
+        /// <summary>
+        /// The treasury (funds) possessed by this country.
+        /// </summary>
         private BankAccount treasury;
+
+        /// <summary>
+        /// The unsold bonds held by this country.
+        /// </summary>
         private BondCollection bonds;
 
-        public Country()
-        {
-            //
-        }
-
+        /// <summary>
+        /// Moves this country to the designated space on the rondel.
+        /// </summary>
+        /// <param name="space">The rondel space to move to.</param>
+        /// <returns>Whether the move to the space was successful.</returns>
         public bool MoveRondelMarker(RondelSpace space)
         {
+            // Check to make sure that the country can pay out when moving to Investor.
             return false;
         }
 
-        public uint PayInvestors()
+        /// <summary>
+        /// Pays all of the investors of this country using the country's treasury.
+        /// </summary>
+        /// <returns>Whether the payout to investors succeeded.</returns>
+        public bool PayInvestors()
         {
-            return 0;
+            return false;
         }
     }
 }
